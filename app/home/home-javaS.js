237cd29235703581e24e7be3805911147a0903f9
JeremiasSosaa <jeremias.sosa@gm2dev.com>
2026-07-14 11:09:01 -0300

    feat(getPointsPlayer):add get score plyer of the api (Not working)

--- app/home/home-javaS.js
+++ app/home/home-javaS.js
@@ -1,5 +1,7 @@
-const apiUrl = "https://quiz-api.cesar-kastli.workers.dev/games";
+const apiUrlGames = "https://quiz-api.cesar-kastli.workers.dev/games";
+const apiUrlPoints = "https://quiz-api.cesar-kastli.workers.dev/scores"; // esperar al nuevo point general en la API
 const yourGames = document.getElementById('yourGames');
+const errorHTML = document.getElementById('opacity');
 
 function welcome() {
     const nickname = localStorage.getItem('nickname')
@@ -16,13 +18,34 @@ function welcome() {
 
 }
 
+async function getPointsPlayer() {
+    try {
+        const respuestaPoints = await fetch(apiUrlPoints);  //no existe el score
+        const points = await respuestaPoints.json();
+        const puntajeHTML = document.getElementById('puntaje');
+
+        points.forEach((puntaje) => {
+
+            if (puntaje.playerName == nickname) {
+                puntajeHTML.innerHTML = `${puntaje.score}`;
+            }
+        });
+    } catch {
+        errorHTML.style.display = 'flex';
+    }
+    if(puntaje.score <= 0){
+        puntajeHTML.innerHTML = `0`;
+    }
+
+}
+
 async function getGames() {
     try {
-        const respuesta = await fetch(apiUrl);
-        const games = await respuesta.json();
+        const respuestaGames = await fetch(apiUrlGames);
+        const games = await respuestaGames.json();
 
         games.forEach((game, index) => {
-            console.log(games); 
+
             const li = document.createElement("li");
             li.innerHTML = `
                     <div id='games'>
@@ -43,11 +66,11 @@ async function getGames() {
             yourGames.appendChild(li);
         });
     } catch (error) {
-        const li = document.createElement("li");
-        li.innerHTML = `Error al conectar con la API: ${error}` //terminar esta aprte
+        errorHTML.style.display = 'flex';
     }
 }
 
-getGames()
 welcome()
+getPointsPlayer()
+getGames()
 
